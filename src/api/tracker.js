36677a031a7657c43dc42bbf4795e36707a66c0c
Kenny Ngo <kenny.ngo.design@gmail.com>
2021-01-27 22:59:27 -0800

    stylized signup section

---
+++ src/api/tracker.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "http://7456878f2a0a.ngrok.io",
+});
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,13 +1,48 @@
+import React from "react";
+
+import trackerApi from "../api/tracker";
 import createDataContext from "./createDataContext";
 
 const authReducer = (state, action) => {
   switch (action.type) {
+    case "add_error":
+      return { ...state, errorMessage: action.payload };
     default:
       return state;
   }
 };
+
+const signup = (dispatch) => {
+  return async ({ email, password }) => {
+    // make an API request to sign up with that email and password
+    // if we sign up, modify our state, say that we are authenticated
+    // If signup fails, we need to reflect error message
+    try {
+      const response = await trackerApi.post("/signup", { email, password });
+      console.log(response.data);
+    } catch (err) {
+      dispatch({
+        type: "add_error",
+        payload: "Something went wrong with signup",
+      });
+    }
+  };
+};
+
+const signin = (dispatch) => {
+  return ({ email, password }) => {
+    /* 
+		try to sign in
+		*/
+  };
+};
+
+const signout = (dispatch) => {
+  return () => {};
+};
+
 export const { Provider, Context } = createDataContext(
   authReducer,
-  {},
-  { isSignedIn: false }
+  { signin, signout, signup },
+  { isSignedIn: false, errorMessage: "" }
 );
